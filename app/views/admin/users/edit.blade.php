5d1cfae2f5de0654e89d28ac91f3c4d23db1078d
Florian Binoeder <florian.binoeder@gmail.com>
2014-12-06 11:52:39 +0100

    Restructure User management

--- app/views/admin/users/edit.blade.php
+++ app/views/admin/users/edit.blade.php
@@ -40,33 +40,45 @@
                     </label>
                 </div>
                 @endif
-                <div class="row">
-                    <div class="col-md-6">
-                        {{ Bootstrap::submit(trans('pagestrings.users_edit_save')) }}
-                        {{ Form::close() }}
-                    </div>
-                        {{ Form::model($user, ['method' => 'PATCH', 'route' => ['admin.users.resend', $user->id]]) }}
-                        {{ Bootstrap::submit(trans('pagestrings.users_edit_reset_password')) }}
-                        {{ Form::close() }}
-                    <div class="col-md-6">
-                    </div>
+                {{ Bootstrap::submit(trans('pagestrings.users_edit_save')) }}
+                {{ Form::close() }}
+
                 </div>
          </div>
          </div>
 
-        <div class="panel panel-danger">
-             <div class="panel-heading">
-                <h3 class="panel-title">{{ trans('pagestrings.users_edit_delete') }}</h3>
-             </div>
-             <div class="panel-body">
-                <div class="row">
-                    <div class="col-md-6 col-md-offset-3">
-                        {{ Form::model($user, ['method' => 'DELETE', 'route' => ['admin.users.destroy', $user->id], 'id' => 'delete_user']) }}
-                            {{ Bootstrap::submit(trans('pagestrings.users_edit_delete'), ['class' => 'btn btn-danger btn-block']) }}
-                        {{ Form::close() }}
-                    </div>
+
+
+        <div class="row">
+            <div class="col-md-6">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                <h3 class="panel-title">{{ trans('pagestrings.users_edit_reset_password') }}</h3>
                 </div>
-             </div>
+                <div class="panel-body">
+                <div class="col-md-6 col-md-offset-3">
+                {{ Form::model($user, ['method' => 'PATCH', 'route' => ['admin.users.resend', $user->id]]) }}
+                {{ Bootstrap::submit(trans('pagestrings.users_edit_reset_password'),['class' => 'btn btn-primary']) }}
+                {{ Form::close() }}
+                </div>
+                </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+            <div class="panel panel-danger">
+                <div class="panel-heading">
+                <h3 class="panel-title">{{ trans('pagestrings.users_edit_delete') }}</h3>
+                </div>
+                <div class="panel-body">
+                <div class="col-md-6 col-md-offset-3">
+                {{ Form::model($user, ['method' => 'DELETE', 'route' => ['admin.users.destroy', $user->id]]) }}
+                {{ Bootstrap::submit(trans('pagestrings.users_edit_delete'), ['class' => 'btn btn-danger']) }}
+                {{ Form::close() }}
+                </div>
+                </div>
+                </div>
+            </div>
+            </div>
         </div>
     </div>
 @stop
--- app/views/admin/users/index.blade.php
+++ app/views/admin/users/index.blade.php
@@ -9,12 +9,12 @@
 @stop
 @section('content') 
     
-    <table class="table table-striped">
+    <table id="users" class="table table-striped">
       <thead>
         <tr>
           <th></th>
-          <th>{{ trans('pagestrings.users_first_name') }}</th>
           <th>{{ trans('pagestrings.users_last_name') }}</th>
+          <th>{{ trans('pagestrings.users_first_name') }}</th>
           <th>{{ trans('pagestrings.users_email') }}</th>
         </tr>
       </thead>
@@ -24,21 +24,52 @@
         <tr>
           <td><a href="{{ action('UsersController@edit', [$user->id]) }}"><i class="fa fa-pencil"></i></a>&nbsp&nbsp
           <a href="{{ action('UsersController@show', [$user->id]) }}"><i class="fa fa-list"></i></a></td>
-          <td>{{ $user->first_name }}</td>
           <td>{{ $user->last_name }}</td>
-          <td>{{ $user->email }}</a></td>
+          <td>{{ $user->first_name }}</td>
+          <td><a href={{ $user->email }}>{{ $user->email }}</a></td>
         </tr>
         @endforeach
       </tbody>
     </table>
       <hr/>
-        <h3>{{ trans('pagestrings.users_index_admins') }}</h3>
-    <ul>
-        @foreach ($admins as $admin)
-        <li><a href="mailto:{{ $admin->email }}">{{$admin->full_name . ": " .$admin->email}}</a></li>
-        @endforeach
+        <h3><br/>{{ trans('pagestrings.users_index_admins') }}</h3>
+        <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>{{ trans('pagestrings.users_last_name') }}</th>
+                  <th>{{ trans('pagestrings.users_first_name') }}</th>
+                  <th>{{ trans('pagestrings.users_email') }}</th>
+                </tr>
+              </thead>
+              <tbody>
+                <h3></h3>
+                @foreach ($admins as $admin)
+                <tr>
+                  <td><a href="{{ action('UsersController@edit', [$admin->id]) }}"><i class="fa fa-pencil"></i></a>&nbsp&nbsp
+                  <a href="{{ action('UsersController@show', [$admin->id]) }}"><i class="fa fa-list"></i></a></td>
+                  <td>{{ $admin->last_name }}</td>
+                  <td>{{ $admin->first_name }}</td>
+                  <td><a href={{ $admin->email }}>{{ $admin->email }}</a></td>
+                </tr>
+                @endforeach
+              </tbody>
+        </table>
     </ul>
 
 
+@stop
+
+@section('javascript')
+    <script type="text/javascript">
+          $(document).ready(function() {
+                  $('#users').dataTable( {
+                      "language": {
+                          "url": "{{ Config::get('app.datatableslocale') }}"
+                      }
+                  } );
+              } );
+
+     </script>
 @stop
 
